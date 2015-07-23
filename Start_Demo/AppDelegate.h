8dd7030a54a77f10b317354a516a9e84edd7a2c9
divine_child <tops@Tops31.local>
2015-07-23 14:42:33 +0530

    Initial Commit

---
+++ Start_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Start_Demo
+//
+//  Created by Tops on 7/23/15.
+//  Copyright (c) 2015 Tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
